506a0e07f2781d9972d9c154c98e4f63c9546e33
kvzon1984 <ivega.josue@gmail.com>
2023-05-18 00:58:22 -0400

    fix: getter, setter, static

--- curso/src/04-POO.ts
+++ curso/src/04-POO.ts
@@ -1,21 +1,46 @@
 /* LAS CLASES ESTAN COMPUESTASZ POR PROPIEDADES (VARIABLES) Y METODOS (FUNCIONES) */
 
+// class Personaje {
+//     //Propiedades
+//     readonly id: number
+//     name: string
+//     nivel: number
+//     private _hp: number
+//     profesion?: string
+
+//     constructor(id: number, name: string, nivel: number, _hp: number) {
+//         this.id = id
+//         this.name = name
+//         this.nivel = nivel
+//         this._hp = _hp
+//     }
+
+//     // Metodos
+
+//     subirNivel(): number {
+//         this.nivel = this.nivel++
+//         return this.nivel
+//     }
+
+//     cambiarHp(cantidad: number): number {
+//         this._hp = this._hp + cantidad
+//         return this._hp
+//     }
+// }
+
+// Simpificando (REFACTOR) la clase Personaje
+
 class Personaje {
-    //Propiedades
-    readonly id: number
-    name: string
-    nivel: number
-    private _hp: number
+
     profesion?: string
+    private static _equipo: number = 1
 
-    constructor(id: number, name: string, nivel: number, _hp: number) {
-        this.id = id
-        this.name = name
-        this.nivel = nivel
-        this._hp = _hp
+    constructor(
+        public readonly id: number,
+        public name: string,
+        public nivel: number,
+        private _hp: number) {
     }
-
-    // Metodos
 
     subirNivel(): number {
         this.nivel = this.nivel++
@@ -26,10 +51,34 @@ class Personaje {
         this._hp = this._hp + cantidad
         return this._hp
     }
+
+    static agregarPersonajeEquipo(): void {
+        Personaje._equipo++
+    }
+
+    static get equipo(): number{
+        return Personaje._equipo
+    }
+
+    // getHP(): number {
+    //     return this._hp
+    // }
+
+    get hp(): number {
+        return this._hp
+    }
+
+    // LO IDEAL ES QUE NO SE OCUPE EL SETTER PARA REAL;IZAR UN CAMBIO EN LA PROPIEDAD, PARA ESO ES MEJOR
+    // OCUPAR UN METODO, LO MEJOR ES OCUPAR LOS SETTER PARA VALIDAR EL VALOR DE DICHA PROPIEDAD
+    set hp(cantidad: number) {
+        this._hp = this._hp + cantidad
+    }
+
+
 }
 
 const personaje = new Personaje(
-    158895250,
+    1,
     'Isaac',
     1,
     100,
@@ -45,8 +94,14 @@ if (personaje instanceof Personaje) {
 
 personaje.profesion = 'Justiciero'
 
-console.log(personaje);
+const personaje1 = new Personaje(2, 'Carlos', 1, 100)
+Personaje.agregarPersonajeEquipo()
 
-console.log('Pruba');
+console.log('_equipo', Personaje.equipo);
 
+console.log(personaje, personaje1);
+console.log(personaje.hp);
+
+personaje.hp = -10
+console.log(personaje.hp);
 
